e3bcfb770542b5261d60eeea1e36a49de081552f
Aviarm <aviram@sogomatic.com>
2026-03-31 11:37:04 +0300

    feat: Add AddonImages and Media collections to Payload CMS configuration and update .gitignore for local media storage

--- frontend/payload.config.ts
+++ frontend/payload.config.ts
@@ -3,7 +3,9 @@ import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import { postgresAdapter } from "@payloadcms/db-postgres";
 import { buildConfig } from "payload";
 import { Admins } from "./collections/Admins";
+import { Media } from "./collections/Media";
 import { he } from "@payloadcms/translations/languages/he";
+import { AddonImages } from "./collections/AddonImages";
 
 export default buildConfig({
   // If you'd like to use Rich Text, pass your editor here
@@ -21,6 +23,13 @@ export default buildConfig({
   i18n: {
     fallbackLanguage: "he",
     supportedLanguages: { he },
+    translations: {
+      he: {
+        general: {
+          collections: "קולקציות",
+        },
+      },
+    },
   },
 
   admin: {
@@ -47,7 +56,7 @@ export default buildConfig({
     admin: "/cms",
   },
   // Define and configure your collections in this array
-  collections: [Admins],
+  collections: [Admins, Media, AddonImages],
 
   // Your Payload secret - should be a complex and secure string, unguessable
   secret: process.env.PAYLOAD_SECRET || "",
