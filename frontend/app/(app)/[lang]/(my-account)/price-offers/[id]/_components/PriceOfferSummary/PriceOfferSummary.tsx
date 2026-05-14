c86a97219a8a773af6d24fb62c6639072bac593e
Aviarm <aviram@sogomatic.com>
2026-05-14 12:50:35 +0300

    feat: add offered price display in PriceOfferSummary and update translations

--- frontend/app/(app)/[lang]/(my-account)/price-offers/[id]/_components/PriceOfferSummary/PriceOfferSummary.tsx
+++ frontend/app/(app)/[lang]/(my-account)/price-offers/[id]/_components/PriceOfferSummary/PriceOfferSummary.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import { formatPrice } from "@/shared/utils/formatPrice";
 import { CarDetailsSection } from "../../../../_components/CarDetailsSection";
 import { CostBreakdownSection } from "../../../../_components/CostBreakdownSection";
 import { IncludedSection } from "../../../../_components/IncludedSection";
 import { RentalSummary } from "../../../../_components/RentalSummary";
 import { usePriceOffer } from "../../_hooks/usePriceOffer";
 import { HeaderSection } from "./HeaderSection";
+import { useTranslations } from "next-intl";
 
 export function PriceOfferSummary({ priceOfferId }: { priceOfferId: number }) {
+  const t = useTranslations("MyAccount.priceOffer.summary.labels");
   const { data: priceOffer } = usePriceOffer(priceOfferId);
 
   return (
@@ -35,6 +38,11 @@ export function PriceOfferSummary({ priceOfferId }: { priceOfferId: number }) {
         totalPrice={priceOffer.totalPrice}
         currencyCode={priceOffer.currencyCode}
       />
+
+      <div className="text-white bg-brand py-3 5 px-5 flex justify-between items-center rounded-xl">
+        <span className="type-paragraph">{t("offeredPrice")}</span>
+        <h4 className="type-h4">{formatPrice(priceOffer.offeredPrice, priceOffer.offeredCurrencyCode)}</h4>
+      </div>
     </div>
   );
 }
