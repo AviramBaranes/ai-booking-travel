df50067bf798994d9fcb9526b4b9ab36ea0a6c8b
Aviarm <aviram@sogomatic.com>
2026-06-19 18:34:28 +0300

    feat: update balance due parameter naming for office and organization handlers

--- backend/services/accounts/handlers/office/update_balance_due.go
+++ backend/services/accounts/handlers/office/update_balance_due.go
@@ -17,8 +17,8 @@ type UpdateOfficeBalanceDueParams struct {
 // UpdateOfficeBalanceDue updates the balance due for an office work for plus or minus the current balance due.
 func (s *OfficeService) UpdateOfficeBalanceDue(ctx context.Context, p UpdateOfficeBalanceDueParams) error {
 	if err := s.query.UpdateOfficeBalanceDue(ctx, db.UpdateOfficeBalanceDueParams{
-		ID:         p.ID,
-		BalanceDue: dbadapters.NumericFromFloat64(p.BalanceChange),
+		ID:    p.ID,
+		Delta: dbadapters.NumericFromFloat64(p.BalanceChange),
 	}); err != nil {
 		rlog.Error("failed to update office balance due", "error", err)
 		return api_errors.ErrInternalError
--- backend/services/accounts/handlers/organization/update_balance_due.go
+++ backend/services/accounts/handlers/organization/update_balance_due.go
@@ -17,8 +17,8 @@ type UpdateOrganizationBalanceDueParams struct {
 // UpdateOrganizationBalanceDue updates the balance due for an organization work for plus or minus the current balance due.
 func (s *OrganizationService) UpdateOrganizationBalanceDue(ctx context.Context, p UpdateOrganizationBalanceDueParams) error {
 	if err := s.query.UpdateOrganizationBalanceDue(ctx, db.UpdateOrganizationBalanceDueParams{
-		ID:         p.ID,
-		BalanceDue: dbadapters.NumericFromFloat64(p.BalanceChange),
+		ID:    p.ID,
+		Delta: dbadapters.NumericFromFloat64(p.BalanceChange),
 	}); err != nil {
 		rlog.Error("failed to update organization balance due", "error", err)
 		return api_errors.ErrInternalError
